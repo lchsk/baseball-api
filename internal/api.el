2fb3bb0883b3ff680bacae56add9436bd9ed60bc
lchsk <mjlechowski@gmail.com>
2019-07-20 19:59:55 +0100

    Add api.el file

---
+++ internal/api.el
@@ -0,0 +1,11 @@
+
+
+GET http://localhost:8000/api/v1/games/2018-03-29/BOS@TBA/
+###
+GET http://localhost:8000/api/v1/games/2018-03-29/BOS@TBA/lineups
+###
+GET http://localhost:8000/api/v1/games/2018-03-29/BOS@TBA/stats
+###
+
+GET http://localhost:8000/api/v1/teams/TBA
+###
