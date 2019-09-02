36672aa12c9118a49f07ee5c204b7e3cd07d4d81
lchsk <mjlechowski@gmail.com>
2019-09-02 21:50:30 +0100

    Add more logging in db connection

--- db_connection.go
+++ db_connection.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 )
@@ -10,10 +11,22 @@ import (
 func getDBConnection() *sql.DB {
 
 	host := os.Getenv("BASEBALL_HOST")
-	port, _ := strconv.Atoi(os.Getenv("BASEBALL_PORT"))
+
+	log.Println("Host:", host)
+
+	port, portErr := strconv.Atoi(os.Getenv("BASEBALL_PORT"))
+
+	if portErr != nil {
+		log.Println("Could not read port env var", portErr)
+	} else {
+		log.Println("Port:", port)
+	}
+
 	user := os.Getenv("BASEBALL_USER")
+	log.Println("User:", user)
 	password := os.Getenv("BASEBALL_PASS")
 	dbname := os.Getenv("BASEBALL_DB")
+	log.Println("dbname:", dbname)
 
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
